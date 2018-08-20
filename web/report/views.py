6300df23f83707e38b95726b93e5137789dd86fd
felexkemboi <felokemboi10@gmail.com>
2018-08-20 16:40:17 +0300

    fetch the records from the database and display them on the browser

--- web/report/views.py
+++ web/report/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Board
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+
+
+def home(request):
+    boards = Board.objects.all() #fetch all records in the database and assign it to boards
+    boards_names = list()  #declare an empty list
+
+    for board in boards:                 #sasa wewe Hassan ni wewe utanieleza hii ya for loop
+        boards_names.append(board.name)
+
+    response_html = '<br>'.join(boards_names)
+
+    return HttpResponse(response_html)
