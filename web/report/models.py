385617dc238dfea03b804aa6fb3befe7c6db7f61
felexkemboi <felokemboi10@gmail.com>
2018-08-20 16:29:20 +0300

    make migrations

--- web/report/models.py
+++ web/report/models.py
@@ -6,21 +6,21 @@ from django.contrib.auth.models import User
 #Each class will be transformed into database tables
 #fields CharField, DateTimeField, etc., are all subclasses of django.db.models.Field and they come included in the Django core – ready to be used.
 class Board(models.Model):
-    name = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=30, unique=True)  #it will enforce the uniqueness of the field at the database level.
     description = models.CharField(max_length=100)
 
 
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
-    board = models.ForeignKey(Board, related_name='topics')
-    starter = models.ForeignKey(User, related_name='topics')
+    board = models.ForeignKey(Board, related_name='topics',on_delete=models.CASCADE,)
+    starter = models.ForeignKey(User, related_name='topics',on_delete=models.CASCADE,)
 
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
-    topic = models.ForeignKey(Topic, related_name='posts')
+    topic = models.ForeignKey(Topic, related_name='posts',on_delete=models.CASCADE,)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
-    created_by = models.ForeignKey(User, related_name='posts')
-    updated_by = models.ForeignKey(User, null=True, related_name='+')
+    created_by = models.ForeignKey(User, related_name='posts',on_delete=models.CASCADE,)
+    updated_by = models.ForeignKey(User, null=True, related_name='+',on_delete=models.CASCADE,)
